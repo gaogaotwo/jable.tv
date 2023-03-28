1983bcff8d7cbecf2bf1b0ec567b0703ef11de23
gaogaotwo <50804820+gaogaotwo@users.noreply.github.com>
2023-03-29 02:48:13 +0800

    curl 请求相关字符串bug

--- main.py
+++ main.py
@@ -5,19 +5,16 @@ from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 import shutil
 import time
-
-##########以下参数需要配置
-# 该网站需要代理相关的操作，需进行相关配置
+# 该网站需要代理相关的操作，需进行相关配置！
 proxy_url = "http://127.0.0.1:10810"
 # 视频下载链接
 url = "https://jable.tv/videos/stars-244/"
-# 线程池数量
-thread_nums = 20
-###########
-
 file_tile = url.split('/')[4]
 req_url = ""
 iv = ""
+# 线程池数量
+thread_nums = 20
+
 # 创建的临时目录
 temp_dir = Path('./temp_dir')
 temp_sol_dir = Path('./temp_sol')
@@ -27,11 +24,8 @@ vedio_dir = Path('./vedio')
 def jable_init(proxy_url, url):
     global req_url
     global iv
-    cmd = 'curl -L -o ./hlsUrl.txt -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (' \
-          'KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36" -H "Referer: https://jable.tv/" -H "Accept-L: zh-CN,' \
-          'zh;q=0.9" -H "Sec-Fetch-User: ?1" -H "Sec-Ch-Ua: \"Not?A_Brand\";v=\"99\", \"Chromium\";v=\"90\", ' \
-          '\"Google  Chrome\";v=\"90\"" -H "Sec-Ch-Ua-Mobile: ?0" -H "Sec-Ch-Ua-Platform: \"Windows\"" -H ' \
-          '"Cache-Control: no-cache" -H "Pragma: no-cache" -x ' + proxy_url + " " + url + ' >hlsUrl.txt '
+    # 相关命令，字符串缩进需注意一下，可能会报错
+    cmd = 'curl -L -o ./hlsUrl.txt -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36" -H "Referer: https://jable.tv/" -H "Accept-L: zh-CN,zh;q=0.9" -H "Sec-Fetch-User: ?1" -H "Sec-Ch-Ua: \"Not?A_Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Google  Chrome\";v=\"90\"" -H "Sec-Ch-Ua-Mobile: ?0" -H "Sec-Ch-Ua-Platform: \"Windows\"" -H "Cache-Control: no-cache" -H "Pragma: no-cache" -x ' + proxy_url + " " + url + ' >hlsUrl.txt'
     print(cmd)
     os.system(cmd)
     with open('./hlsUrl.txt', 'rb') as f:
@@ -41,13 +35,8 @@ def jable_init(proxy_url, url):
     # 修复一下，URL链接并不确定，需修复
     for i in range(len(rls) - 1):
         req_url = req_url + rls[i] + "/"
-
-    cmd = r'curl -L  -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
-          r'Chrome/108.0.0.0 Safari/537.36" -H "Referer: https://jable.tv/" -H "Accept-L: zh-CN,zh;q=0.9" -H ' \
-          r'"Sec-Fetch-User: ?1" -H "Sec-Ch-Ua: \"Not?A_Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Google  ' \
-          r'Chrome\";v=\"90\"" -H "Sec-Ch-Ua-Mobile: ?0" -H "Sec-Ch-Ua-Platform: \"Windows\"" -H "Cache-Control: ' \
-          r'no-cache" -H "Pragma: no-cache" -x ' + proxy_url + "  " + "\"" + \ 
-          hlsUrl[0] + "\"" + ' > ./ts.txt'
+        
+    cmd = 'curl -L  -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36" -H "Referer: https://jable.tv/" -H "Accept-L: zh-CN,zh;q=0.9" -H "Sec-Fetch-User: ?1" -H "Sec-Ch-Ua: \"Not?A_Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Google  Chrome\";v=\"90\"" -H "Sec-Ch-Ua-Mobile: ?0" -H "Sec-Ch-Ua-Platform: \"Windows\"" -H "Cache-Control: no-cache" -H "Pragma: no-cache" -x ' + proxy_url + "  " +"\"" +hlsUrl[0] +"\"" +' > ./ts.txt'
     print(cmd)
     os.system(cmd)
     with open('./ts.txt', 'rb') as f:
@@ -57,33 +46,15 @@ def jable_init(proxy_url, url):
     m3u8_iv = re.findall(r'IV=0x([0-9a-fA-F]+)', text.decode('utf-8'))
     iv = bytes.fromhex(m3u8_iv[0])
     # 获取m3u8 加密密钥
-    cmd = 'curl -L -o ./temp_dir/m3u8.key -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) ' \
-          'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36" -H "Referer: https://jable.tv/" -H ' \
-          '"Accept-L: zh-CN,zh;q=0.9" -H "Sec-Fetch-User: ?1" -H "Sec-Ch-Ua: \"Not?A_Brand\";v=\"99\", ' \
-          '\"Chromium\";v=\"90\", \"Google  Chrome\";v=\"90\"" -H "Sec-Ch-Ua-Mobile: ?0" -H "Sec-Ch-Ua-Platform: ' \
-          '\"Windows\"" -H "Cache-Control: no-cache" -H "Pragma: no-cache" -x  ' + proxy_url + " " + "\"" + req_url + \ 
-          m3u8_key[0] + "\"" + ' > ./temp_dir/m3u8.key'
+    cmd = 'curl -L -o ./temp_dir/m3u8.key -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36" -H "Referer: https://jable.tv/" -H "Accept-L: zh-CN,zh;q=0.9" -H "Sec-Fetch-User: ?1" -H "Sec-Ch-Ua: \"Not?A_Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Google  Chrome\";v=\"90\"" -H "Sec-Ch-Ua-Mobile: ?0" -H "Sec-Ch-Ua-Platform: \"Windows\"" -H "Cache-Control: no-cache" -H "Pragma: no-cache" -x  ' + proxy_url + " " + "\"" + req_url + m3u8_key[0]+"\"" + ' > ./temp_dir/m3u8.key'
     os.system(cmd)
     return m3u8_ts
 
 
 def get_content(u):
-    cotent = "\"" + req_url + u + "\""
-    cmd = 'curl --connect-timeout 15 --retry 5 -L -o ./temp_dir/' + u + '-H "User-Agent: Mozilla/5.0 (Windows NT ' \
-                                                                        '10.0; Win64; x64) AppleWebKit/537.36 (KHTML, ' \
-                                                                        'like Gecko) Chrome/108.0.0.0 Safari/537.36" ' \
-                                                                        '-H "Referer: https://jable.tv/" -H ' \
-                                                                        '"Accept-L: zh-CN,zh;q=0.9" -H ' \
-                                                                        '"Sec-Fetch-User: ?1" -H "Sec-Ch-Ua: ' \
-                                                                        '\"Not?A_Brand\";v=\"99\", ' \
-                                                                        '\"Chromium\";v=\"90\", \"Google  ' \
-                                                                        'Chrome\";v=\"90\"" -H "Sec-Ch-Ua-Mobile: ?0" ' \
-                                                                        '-H "Sec-Ch-Ua-Platform: \"Windows\"" -H ' \
-                                                                        '"Cache-Control: no-cache" -H "Pragma: ' \
-                                                                        'no-cache" -x ' + proxy_url + " " + cotent + \
-          ' > ./temp_dir/' + u 
+    cotent = "\""+req_url + u+"\""
+    cmd = 'curl --connect-timeout 15 --retry 5 -L -o ./temp_dir/' + u + ' -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36" -H "Referer: https://jable.tv/" -H "Accept-L: zh-CN,zh;q=0.9" -H "Sec-Fetch-User: ?1" -H "Sec-Ch-Ua: \"Not?A_Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Google  Chrome\";v=\"90\"" -H "Sec-Ch-Ua-Mobile: ?0" -H "Sec-Ch-Ua-Platform: \"Windows\"" -H "Cache-Control: no-cache" -H "Pragma: no-cache" -x ' + proxy_url + " " + cotent + ' > ./temp_dir/' + u
     os.system(cmd)
-
 
 def m3u8_fix(m3u8_ts):
     key = open('./temp_dir/m3u8.key', 'rb')
@@ -99,9 +70,8 @@ def m3u8_fix(m3u8_ts):
             print("ts子文件未成功下载")
     key.close()
 
-
 def file_merging(m3u8_ts):
-    with open('./vedio/' + file_tile + ".mp4", 'wb') as f:
+    with open('./vedio/' + file_tile + ".mp4" , 'wb') as f:
         for ts in m3u8_ts:
             # 异常捕捉，存在部分子视频片段未成功下载
             try:
